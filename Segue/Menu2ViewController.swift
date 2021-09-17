8633c325ae97a4b90c300aa218a10f7153ddaa10
marcoalonso <marcoalonsorota@hotmail.com>
2021-09-17 09:39:50 -0500

    3-vistas-segues

---
+++ Segue/Menu2ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  Menu2ViewController.swift
+//  Segue
+//
+//  Created by marco rodriguez on 17/09/21.
+//
+
+import UIKit
+
+class Menu2ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func regresarBtn(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+}
---
+++ Segue/RegistroViewController.swift
@@ -0,0 +1,32 @@
+//
+//  RegistroViewController.swift
+//  Segue
+//
+//  Created by marco rodriguez on 17/09/21.
+//
+
+import UIKit
+
+class RegistroViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func regresarButton(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
